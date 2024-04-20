06f1c549c8e482888440fc02b59eb1921a95e963
Devyatyi9 <devyatyi9@gmail.com>
2024-04-20 15:27:29 +0400

    Update .usm_build.bat

--- .usm_build.bat
+++ .usm_build.bat
@@ -1,1 +1,4 @@
+@echo off
+title .usm_build
 "./distr/USM_Builder/USM_builder.exe" "usm_builder_input" "distr/ffmpeg/bin/ffmpeg.exe" "distr/ffmpeg/bin/ffprobe.exe" "distr/Scaleform VideoEncoder/medianocheH264.exe" "usm_builder_output"
+pause
