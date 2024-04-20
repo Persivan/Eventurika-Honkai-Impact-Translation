5486f39fe4335254b189399b344a43bd6c29e180
Devyatyi9 <devyatyi9@gmail.com>
2024-04-20 15:02:03 +0400

    Update .usm_build.bat

--- .usm_build.bat
+++ .usm_build.bat
@@ -1,1 +1,1 @@
-"./distr/USM_Builder/USM_builder.exe" "usm_builder_input" "distr/ffmpeg/bin/ffmpeg.exe" "distr/ffmpeg/bin/ffprobe.exe" "distr/Scaleform VideoEncoder/medianocheH264.exe" "usm_builder_output" "distr/USM_subs_toolbox.exe"
+"./distr/USM_Builder/USM_builder.exe" "usm_builder_input" "distr/ffmpeg/bin/ffmpeg.exe" "distr/ffmpeg/bin/ffprobe.exe" "distr/Scaleform VideoEncoder/medianocheH264.exe" "usm_builder_output"
